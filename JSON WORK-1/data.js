e0548d06defeec472b0d9201e76e4b870447bb7b
Abdul Wahab Amir <83786802+A-Wahab-Aamir@users.noreply.github.com>
2024-07-04 10:32:26 +0500

    JSON WORK

---
+++ JSON WORK-1/data.js
@@ -0,0 +1,39 @@
+var data=[
+    {
+        "pro_name":"Techno Mobile Spark",
+        "price":"120000",
+        "color":"red",
+        "ram":"64GB",
+        "camera_pixcels":"120px",
+        "image":"img/1.jpg", 
+        "url":"pro1.html"
+
+    }, 
+    {
+        "pro_name":"Nokia Mobile Spark",
+        "price":"120000",
+        "color":"red",
+        "ram":"64GB",
+        "camera_pixcels":"120px",
+        "image":"img/2.jpg",
+        "url":"pro2.html"
+    }, 
+    {
+        "pro_name":"Vevo Mobile Spark",
+        "price":"120000",
+        "color":"red",
+        "ram":"64GB",
+        "camera_pixcels":"120px",
+        "image":"img/3.jpg",
+        "url":"pro3.html"
+    },
+    {
+        "pro_name":"Vevo Mobile Spark",
+        "price":"120000",
+        "color":"red",
+        "ram":"64GB",
+        "camera_pixcels":"120px",
+        "image":"img/4.jpg",
+        "url":"pro4.html"
+    }
+]
